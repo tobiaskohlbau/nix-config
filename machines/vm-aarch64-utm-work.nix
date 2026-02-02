ab4dad9bc13a397dc8f92a845a0c62e5e184b1f5
Tobias Kohlbau <tobias@kohlbau.de>
2026-02-02 07:58:26 +0100

    limit mtu on work machine

--- machines/vm-aarch64-utm-work.nix
+++ machines/vm-aarch64-utm-work.nix
@@ -12,6 +12,8 @@
   ];
 
   networking.enableIPv6 = false;
+  networking.interfaces.enp0s1.mtu = 1340;
+  virtualisation.docker.extraOptions = "--mtu 1340";
 
   environment.pathsToLink = [
     "/share/applications"
